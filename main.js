906c2a11913aa10cb7e0d4e92d31a36fd42bffc8
= <=>
2021-08-18 17:33:13 -0300

    Mysql issues

---
+++ main.js
@@ -0,0 +1,30 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+function createWindow(){
+
+    const win = new BrowserWindow({width: 800, height: 600, autoHideMenuBar: true});
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, "src/index.html"),
+        protocol: 'file', 
+        slashes: true
+    }));
+
+    win.webContents.openDevTools();
+
+}
+
+app.whenReady().then( () => {
+    createWindow();
+
+    app.on('activate', () => {
+        if(BrowserWidow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') app.quit();
+});
+
+
---
+++ src/index.js
@@ -0,0 +1,72 @@
+const form = document.querySelector('form');
+const buttonOne = document.querySelectorAll('a')[0];
+const buttonTwo = document.querySelectorAll('a')[1];
+const submitButton = document.querySelector('#submit');
+const passwordForgotten = document.querySelector('#passwordForgotten');
+
+buttonOne.addEventListener("click", () =>{
+
+    if(!document.querySelector('#anchor')){
+        const a = document.createElement('a');
+        a.appendChild(document.createTextNode('Forgot your password?'));
+        a.setAttribute('id', 'anchor');
+        a.setAttribute('href', '');
+        passwordForgotten.appendChild(a);
+    }
+
+    if(document.querySelector('#nick')){
+        document.querySelector('#nick').remove();
+    }
+
+    buttonTwo.id = 'isNotSelected';
+    buttonOne.removeAttribute('id');
+    submitButton.setAttribute('value', 'Sign In');
+
+});
+
+buttonTwo.addEventListener('click', () =>{
+
+    if(!document.querySelector('#nick')){
+        const input = document.createElement('input');
+        input.setAttribute('type', 'text');
+        input.setAttribute('placeholder', 'Enter your Nickname')
+        input.setAttribute('id', 'nick');
+        form.insertBefore(input, document.querySelectorAll('input')[0]);
+    }
+
+    if(document.querySelector('#anchor')){
+        document.querySelector('#anchor').remove();
+    }
+
+    buttonOne.id = 'isNotSelected';
+    buttonTwo.removeAttribute('id');
+    submitButton.setAttribute('value', 'Sign Up');
+
+});
+
+submitButton.addEventListener('click', () => {
+
+    const mysql = require('mysql');
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: null,
+        database: 'matemagica'
+    });
+
+    connection.connect((error) =>{
+        if(error){
+            
+            return console.log("Connection faild!");
+        }
+
+        console.log('Succesfully connected!');
+
+    });
+
+    connection.end(() =>{
+        console.log('Connection closed!');
+    });
+
+});
